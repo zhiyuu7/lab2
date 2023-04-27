18592d16a8bce16bc101c77cd6517502f099b60e
zhiyu <lzhiyuu7@gmail.com>
2023-04-27 11:57:10 +0800

    pushing bmi.py to github

---
+++ bmi.py
@@ -0,0 +1,15 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    bmi = weight / (height * height)
+    if bmi < 18.5:
+        classification = "Under Weight"
+    if bmi >= 18.5 and bmi <= 25.0:
+        classification = "Normal Weight"
+    if bmi > 25.0:
+        classification = "Over Weight"
+
+    print("BMI value = " + str(bmi))
+    print(classification)
+calculate_bmi(weight=57, height=1.73)
