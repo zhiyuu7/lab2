a148efa9e170991e0657fa55cf8891808a7ca1ad
zhiyu <lzhiyuu7@gmail.com>
2023-06-13 15:35:30 +0800

    adding return values to bmi

--- ex1_prac.py
+++ ex1_prac.py
@@ -14,10 +14,13 @@ def calculate_bmi(height,weight):
 def bmi_range(bmi):
     if bmi > 25.0:
         print("BMI: " + str(round(bmi, 2)) + ", Over Weight")
+        return 1
     elif bmi >= 18.5 and bmi <= 25.0:
         print("BMI: " + str(round(bmi, 2)) + ", Normal Weight")
+        return 0
     else:
         print("BMI: " + str(round(bmi, 2)) + ", Under Weight")
+        return -1
 
 
 if __name__ == "__main__":
