2e1ee8c2910cfd90b5c04ce9963b34e9393e3166
zhiyu <lzhiyuu7@gmail.com>
2023-06-15 02:55:55 +0800

    updating lab 2

--- ex1_prac.py
+++ ex1_prac.py
@@ -1,17 +1,10 @@
-def main():
-    print("Lab 2 Ex 1 (bmi) prac")
-    bmi = calculate_bmi(1.73,57)
-    bmi_range(bmi)
-
 def calculate_bmi(height,weight):
     print("Height: " + str(height))
     print("Weight: " + str(weight))
     bmi = float(weight) / float(height * height)
     print("BMI = " + str(round(bmi,2)))
-    return bmi
 
 
-def bmi_range(bmi):
     if bmi > 25.0:
         print("BMI: " + str(round(bmi, 2)) + ", Over Weight")
         return 1
@@ -21,7 +14,3 @@ def bmi_range(bmi):
     else:
         print("BMI: " + str(round(bmi, 2)) + ", Under Weight")
         return -1
-
-
-if __name__ == "__main__":
-    main()
